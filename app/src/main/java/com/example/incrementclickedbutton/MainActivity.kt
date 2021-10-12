8d731e6e096589454ae53b228deb380f9e616587
salim-maula <salim-maula>
2021-10-12 15:51:40 +0700

    Merubah data, menjadi saveInstance

--- app/src/main/java/com/example/incrementclickedbutton/MainActivity.kt
+++ app/src/main/java/com/example/incrementclickedbutton/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.example.incrementclickedbutton
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = MainActivity::class.java.simpleName
+
     private var score = 0
+
     /*Jika Anda tidak ingin menginisialisasi variabel dalam konstruktor,
-    Anda ingin menginisialisasinya nanti dan jika Anda dapat menjamin
-    inisialisasi sebelum menggunakannya, maka deklarasikan
-    variabel tersebut dengan kata kunci*/
+   Anda ingin menginisialisasinya nanti dan jika Anda dapat menjamin
+   inisialisasi sebelum menggunakannya, maka deklarasikan
+   variabel tersebut dengan kata kunci*/
     private lateinit var gameScoreTextView: TextView
     private lateinit var timeLeftTextView: TextView
     private lateinit var tapMeButton: Button
 
     private var gameStarted = false
+
     private lateinit var countDownTimer: CountDownTimer
     private var initialCountDown: Long = 60000
     private var countDownInterval: Long = 1000
@@ -28,22 +34,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d(TAG, "onCreate called. Score is: $score")
+
+        // connect views to variables
+        // 1
         gameScoreTextView = findViewById(R.id.game_score_text_view)
         timeLeftTextView = findViewById(R.id.time_left_text_view)
         tapMeButton = findViewById(R.id.tap_me_button)
-
+        // 2
         tapMeButton.setOnClickListener { incrementScore() }
 
-        resetGame()
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeft = savedInstanceState.getInt(TIME_LEFT_KEY)
+            restoreGame()
+        } else {
+            resetGame()
+        }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putInt(TIME_LEFT_KEY, timeLeft)
+        countDownTimer.cancel()
+
+        Log.d(TAG, "onSaveInstanceState: Saving Score: $score & Time Left: $timeLeft")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy called.")
     }
 
     private fun incrementScore() {
 
-        if (!gameStarted){
+        if (!gameStarted) {
             startGame()
         }
-        // increment score logic
+
         score++
 
         val newScore = getString(R.string.your_score, score)
@@ -51,17 +83,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun resetGame() {
-        // reset game logic
+        // 1
         score = 0
+
         val initialScore = getString(R.string.your_score, score)
         gameScoreTextView.text = initialScore
 
         val initialTimeLeft = getString(R.string.time_left, 60)
         timeLeftTextView.text = initialTimeLeft
 
-        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval){
+        // 2
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            // 3
             override fun onTick(millisUntilFinished: Long) {
-                timeLeft = millisUntilFinished.toInt()/1000
+                timeLeft = millisUntilFinished.toInt() / 1000
 
                 val timeLeftString = getString(R.string.time_left, timeLeft)
                 timeLeftTextView.text = timeLeftString
@@ -72,7 +107,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // 4
         gameStarted = false
+    }
+
+    private fun restoreGame() {
+
+        val restoredScore = getString(R.string.your_score, score)
+        gameScoreTextView.text = restoredScore
+
+        val restoredTime = getString(R.string.time_left, timeLeft)
+        timeLeftTextView.text = restoredTime
+
+        countDownTimer = object : CountDownTimer((timeLeft * 1000).toLong(), countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString = getString(R.string.time_left, timeLeft)
+                timeLeftTextView.text = timeLeftString
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+        }
+
+        countDownTimer.start()
+        gameStarted = true
     }
 
     private fun startGame() {
@@ -81,8 +143,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun endGame() {
-        Toast.makeText(this, getString(R.string.game_over_message,
-            score), Toast.LENGTH_LONG).show()
+        Toast.makeText(this, getString(R.string.game_over_message, score), Toast.LENGTH_LONG).show()
         resetGame()
+    }
+
+    //jika kita menginginkan menulis sebuah fungsi atau member variable
+    //    disuatu kelas agar bisa dipanggil tanpa melalui sebuah objek, kita dapat
+    //    melakukannya dengan menulis member atau methid tersebut di dalam companion object
+    //    suatu kelas
+    companion object {
+        private const val SCORE_KEY = "SCORE_KEY"
+        private const val TIME_LEFT_KEY = "TIME_LEFT_KEY"
     }
 }
